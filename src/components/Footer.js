e70cd9385c4c77464f0e718970238d3dba7297ef
Rajibul <rajibulrahamanshaikh@gmail.com>
2023-09-04 17:01:47 +0530

    added carousel

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
   return (
     <div>
-      Footer
+      <footer classname="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div classname="col-md-4 d-flex align-items-center">
+      <Link to="/" classname="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+      </Link>
+      <span classname="text-muted d-flex align-items-center justify-content-center">© 2023 GoFood, Inc</span>
+    </div>
+
+    <ul classname="nav col-md-4 justify-content-end list-unstyled d-flex">
+    </ul>
+  </footer>
     </div>
   )
 }
--- src/screen/Home.js
+++ src/screen/Home.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Temp from '../components/Temp'
+import Carousel from '../components/Carousel'
 
 
 export default function Home() {
   return (
     <div>
       <div><Navbar /></div>
+      <div>
+        <Carousel></Carousel>
+      </div>
       <div>
         <Temp></Temp>
       </div>
